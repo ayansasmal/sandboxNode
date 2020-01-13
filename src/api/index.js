ec55cbb2354bda835301c99899286ede46b667a1
Ayan Sasmal <ayandelhi@gmail.com>
2020-01-13 20:50:14 +1100

    updated user create module

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import { initializeLogger } from "../utils/logger";
 import Login from "../db/operations/login";
-import User from "../db/operations/user";
+import Users from "../db/operations/user";
 
 const logger = initializeLogger("api");
 
@@ -25,15 +25,33 @@ const loginUser = (req, res) => {
 
 const createUser = (req, res) => {
   logger.debug("Creating user");
-  User.create(req.body)
-    .then(data => {
-      res.status(201);
-      res.json({ status: `User created with id ${data._id}` });
+  if (req.body) {
+    Users.isUsernameAvailable({
+      "identifier.username": req.body.identifier.username
     })
-    .catch(err => {
-      logger.error(err);
-      res.json(err);
-    });
+      .then(isValidResponse => {
+        logger.debug(
+          `User name is valid and available ${JSON.stringify(isValidResponse)}`
+        );
+        Users.create(req.body)
+          .then(data => {
+            logger.debug(`${JSON.stringify(data)}`);
+            logger.debug(`Created User with id ${data._id}`);
+            res.status(201);
+            res.json({ status: `User created with id ${data._id}` });
+          })
+          .catch(err => {
+            logger.error(`Unable to create user ${JSON.stringify(err)}`);
+            res.status(400);
+            res.json(err);
+          });
+      })
+      .catch(err => {
+        logger.error(err);
+        res.status(400);
+        res.json(err);
+      });
+  }
 };
 
 export default { healthCheck, loginUser, createUser };
