671234ace9a14c9a8eeefa855f25e8bf702d7e6f
Ayan Sasmal <ayandelhi@gmail.com>
2020-04-13 14:58:08 +1000

    added jest dependencies and environment configs

--- src/db/handler.js
+++ src/db/handler.js
@@ -1,41 +1,44 @@
 import { connect, connection } from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import { initializeLogger } from "../utils/logger";
-import { configurations } from "./../config";
 
-const mongod = new MongoMemoryServer({
-  instance: { port: 53005, dbName: "dastkar-app" }
-});
 const logger = initializeLogger("handler-js");
 
 /**
  * Connect to the in-memory database.
  */
 export async function connectDatabase() {
-  logger.debug(`DB Configuration :: ${process.env.DB_SRC}`);
-  let uri = configurations.MONGO_ONLINE_URL.replace(
-    "<username>",
-    configurations.MONGO_CREDS.SUPER.USERNAME
-  )
-    .replace("<password>", configurations.MONGO_CREDS.SUPER.PASSWORD)
-    .replace("<dbname>", configurations.DB_NAME);
+  let uri =
+    process.env.ENV === "prod"
+      ? process.env.MONGO_ONLINE_URL.replace(
+          "<username>",
+          process.env.MONGO_CREDS.SUPER.USERNAME
+        )
+          .replace("<password>", process.env.MONGO_CREDS.SUPER.PASSWORD)
+          .replace("<dbname>", process.env.DB_NAME)
+      : undefined;
 
-  if (
-    process.env.DB_SRC &&
-    process.env.DB_SRC === configurations.LOCAL_SWITCH_KEYWORD
-  ) {
-    logger.debug("Connecting to local DB");
-    uri = await mongod.getConnectionString();
+  try {
+    if (process.env.ENV === "dev") {
+      logger.debug("Connecting to local DB");
+      const mongod = new MongoMemoryServer({
+        instance: { port: 53005, dbName: process.env.DB_NAME },
+      });
+      uri = await mongod.getConnectionString();
+    }
+
+    const mongooseOpts = {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    };
+
+    await connect(uri, mongooseOpts);
+    logger.debug(`connected to db at ${uri}`);
+  } catch (err) {
+    logger.err("Unable to connect to DB");
+    logger.error(err);
   }
-
-  const mongooseOpts = {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useFindAndModify: false
-  };
-
-  await connect(uri, mongooseOpts);
-  logger.debug(`connected to db at ${uri}`);
 }
 
 /**
